8479ba94057e34d4b44dd0399a47b034551baeab
BARONYXZ <krumalmarek@gmail.com>
2024-12-25 10:08:39 +0100

    Initial commit for vite branch

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import NeuralNetworkCanvas from "./NeuralNetworkCanvas";
+
+function App() {
+  return (
+    <>
+      <main className="container mx-auto">
+        <NeuralNetworkCanvas />
+      </main>
+    </>
+  );
+}
+
+export default App;
